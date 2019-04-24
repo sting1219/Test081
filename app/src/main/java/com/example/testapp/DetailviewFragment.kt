23955dcd7b1520a1b5b16ce6a12054ae4abe1cea
dylee <sting1219@gmail.com>
2019-04-24 17:37:11 +0900

    8일차 - 그리드 화면 출력까지.

--- app/src/main/java/com/example/testapp/DetailviewFragment.kt
+++ app/src/main/java/com/example/testapp/DetailviewFragment.kt
@@ -7,11 +7,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.testapp.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailviewFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        firestore = FirebaseFirestore.getInstance()
 
         var view =  LayoutInflater.from(inflater.context).inflate(R.layout.fragment_detail,container,false)
         view.detailviewfragment_recyclerview.adapter = DetailRecyclerviewAdapter()
@@ -20,22 +28,87 @@ class DetailviewFragment : Fragment(){
         return view
     }
     inner class DetailRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        val contentDTOs : ArrayList<ContentDTO>
+        val contentUidList : ArrayList<String>
+
+        init {
+            contentDTOs = ArrayList()
+            contentUidList = ArrayList()
+
+            var uid = FirebaseAuth.getInstance().currentUser?.uid
+
+            firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                contentUidList.clear()
+
+                for(snapshot in querySnapshot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item)
+                    contentUidList.add(snapshot.id)
+                }
+
+                notifyDataSetChanged()
+            }
+
+        }
         override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
             var view = LayoutInflater.from(p0.context).inflate(R.layout.item_detail,p0,false)
 
             return CustomViewHolder(view)
         }
 
-        inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view)
+        private inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view)
 
         override fun getItemCount(): Int {
-            return 3
+            return contentDTOs.size
         }
 
-        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
+            val viewHolder = (holder as CustomViewHolder).itemView
+            //유저 아이디
+            viewHolder.detailviewitem_profile_textview.text = contentDTOs!![position].userId
+
+            //이미지
+            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewHolder.detailviewitem_imageview_content)
+
+            //설명 텍스트
+            viewHolder.detailviewitem_explain_textview.text = contentDTOs!![position].explain
+
+            //좋아요 카운터 설정
+            viewHolder.detailviewitem_favoritecounter_textview.text = "좋아요 " + contentDTOs!![position].favoriteCount + "개"
+            var uid = FirebaseAuth.getInstance().currentUser!!.uid
+            viewHolder.detailviewitem_favorite_imageview.setOnClickListener{
+                favoriteEvent(position)
+            }
+
+            //좋아요를 클릭
+            if(contentDTOs!![position].favorites.containsKey(uid)){
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
+            }else{ //클릭하지 않았을경우
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
         }
 
+        private fun favoriteEvent(position: Int){
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction{
+                transaction ->
+                var uid = FirebaseAuth.getInstance().currentUser!!.uid
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
 
+                //좋아요 관련
+                if(contentDTO!!.favorites.containsKey(uid)){
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount - 1
+                    contentDTO?.favorites.remove(uid)
+
+                }else {
+                    contentDTO?.favorites[uid] = true
+                    contentDTO?.favoriteCount = contentDTO.favoriteCount + 1
+                }
+                transaction.set(tsDoc,contentDTO)
+            }
+        }
     }
 }
